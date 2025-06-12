3c3b113ed2c2feb7c33e322350d9376b7943014d
04arvind <arvind42005@gmail.com>
2025-06-12 13:41:39 +0530

    ignore file is move to backend folder

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,38 @@
+import User from "../models/User.js"
+
 export async function signup(req,res){
-    res.send("Signup Route");
+    // res.send("Signup Route");
     const {email,password,fullName} = req.body;
+    try {
+        if(!email|| !password|| !fullName){
+            return res.status(400).json({message:"All fields are required"});
+        }    
+        if(password.length<6){
+            return res.status(400).json({message:"password must be at least 6 characters. "});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message:"Invalid email format"});
+        }
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message:"Email already exists, please use a different one"});
+        }
+
+        const idx = Math.floor(Math.random()*100)+1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser = new User.create({
+            email,
+            fullName,
+            password,
+            profilePic:randomAvatar,
+        })
+
+    } catch (error) {
+        
+    }
 }
 export async function login(req,res){
     res.send("Login Route");
