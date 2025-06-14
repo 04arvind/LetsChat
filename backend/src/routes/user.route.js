7cacd83ff292dbdc0455372c58c110fa20b82b8a
04arvind <arvind42005@gmail.com>
2025-06-15 01:03:53 +0530

    user router is done

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getMyFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -12,5 +12,8 @@ router.get("/friends",getMyFriends)
 
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests",getMyFriendRequests);
+router.get("/outgoing-friend-requests",getOutgoingFriendReqs);
 
 export default router; 
