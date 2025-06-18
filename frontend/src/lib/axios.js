a6e47e91e35f9737c2aba43eea787eb0373443cf
04arvind <arvind42005@gmail.com>
2025-06-18 11:59:40 +0530

    frontend setup is done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseurl: "http://localhost:5173/api",
+    baseURL: "http://localhost:5001/api",
     withCredentials: true, // send the cookies with the request
 })
